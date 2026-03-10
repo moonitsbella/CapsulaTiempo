91f84debea18a1e44b613f4e21bf4e2d8c077cf7
isabe <isabe@Bella.home>
2026-03-10 15:43:33 +0100

    "Mejora visual con asteriscos"

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -1,4 +1,8 @@
 
 public class Mensaje {
-
+	public static void main(String[] args) {
+        System.out.println("***********************************");
+        System.out.println("MENSAJE DEL FUTURO: ¡HOLA MUNDO!");
+        System.out.println("***********************************");
+    }
 }
